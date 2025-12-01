a62275e42890a372e8fd3b2f45c484a58213f993
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2025-12-01 20:44:49 +0100

    reboot

--- src/types/auth.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@supabase/supabase-js'
-
-export interface AuthContextType {
-  user: User | null
-  loading: boolean
-  signIn: (email: string, password: string) => Promise<void>
-  signUp: (email: string, password: string) => Promise<void>
-  signOut: () => Promise<void>
-}
